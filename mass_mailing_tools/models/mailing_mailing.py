65806c2ee6ac0e8997f40a4c2d403783b9289ba7
Thibault Delavallée <tde@odoo.com>
2022-06-14 16:42:48 +0200

    mailign tools: unsubscribe

--- mass_mailing_tools/models/mailing_mailing.py
+++ mass_mailing_tools/models/mailing_mailing.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import werkzeug
+
 from odoo import models, _
 
 
@@ -9,3 +11,31 @@ class Mailing(models.Model):
 
     def action_send_statistics(self):
         return self._action_send_statistics()
+
+    def action_unsubscribe(self):
+        self.ensure_one()
+        base_url = '/mailing/%s/unsubscribe' % self.id
+        if self.mailing_on_mailing_list:
+            document_id = self.env['mailing.contact'].search([('email_normalized', '=', self.env.user.email_normalized)]).ids[0]
+        else:
+            model = self.env[self.mailing_model_real]
+            if 'email_normalized' in model:
+                document_id = model.search([('email_normalized', '=', self.env.user.email_normalized)]).ids[0]
+            elif 'email_from' in model:
+                document_id = model.search([('email_from', 'ilike', self.env.user.email_normalized)]).ids[0]
+            elif 'email' in model:
+                document_id = model.search([('email', 'ilike', self.env.user.email_normalized)]).ids[0]
+            else:
+                document_id = model.search(self._parse_mailing_domain()).ids[0]
+
+        params = werkzeug.urls.url_encode({
+            'document_id': document_id,
+            'email': self.env.user.email_normalized,
+            'hash_token': self._generate_mailing_recipient_hash(document_id, self.env.user.email_normalized),
+        })
+        return {
+            'type': 'ir.actions.act_url',
+            'name': "Custom unsubscribe",
+            'target': 'new',
+            'url': '%s?%s' % (base_url, params),
+        }
